d02616736634ece93f811ec67ca1c709497755d5
Abood Magdy <c.aboodmagdy@gmail.com>
2025-05-31 15:46:19 +0300

    feat: add user view with infolist

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -7,6 +7,10 @@ use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\ImageColumn;
@@ -83,6 +87,7 @@ class UserResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -104,6 +109,7 @@ class UserResource extends Resource
             'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
             'edit' => Pages\EditUser::route('/{record}/edit'),
+            'view' => Pages\ViewUser::route('/{record}'),
         ];
     }
 
@@ -114,6 +120,108 @@ class UserResource extends Resource
     public static function getNavigationGroup(): ?string
     {
         return __('Users');
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Section::make(__('Personal Information'))
+                    ->translateLabel()
+                    ->icon('heroicon-o-user')
+                    ->collapsible()
+                    ->schema([
+                        ImageEntry::make('avatar_url')
+                            ->label('Avatar')
+                            ->translateLabel()
+                            ->height(120)
+                            ->width(120)
+                            ->circular()
+                            ->defaultImageUrl(url('/images/default-avatar.png'))
+                            ->columnSpan(1),
+                        
+                        \Filament\Infolists\Components\Grid::make(2)
+                            ->schema([
+                                TextEntry::make('name')
+                                    ->translateLabel()
+                                    ->icon('heroicon-o-user')
+                                    ->size('lg')
+                                    ->weight('bold')
+                                    ->color('primary'),
+                                
+                                TextEntry::make('email')
+                                    ->translateLabel()
+                                    ->icon('heroicon-o-envelope')
+                                    ->copyable()
+                                    ->copyMessage('Email copied!')
+                                    ->color('gray'),
+                            ])
+                            ->columnSpan(2),
+                    ])
+                    ->columns(3),
+
+                Section::make(__('Access & Permissions'))
+                    ->translateLabel()
+                    ->icon('heroicon-o-shield-check')
+                    ->collapsible()
+                    ->schema([
+                        TextEntry::make('roles.name')
+                            ->translateLabel()
+                            ->label('Roles')
+                            ->badge()
+                            ->color('success')
+                            ->icon('heroicon-o-key')
+                            ->separator(',')
+                            ->columnSpanFull(),
+                        
+                        TextEntry::make('created_at')
+                            ->translateLabel()
+                            ->label('Member Since')
+                            ->icon('heroicon-o-calendar')
+                            ->dateTime('F j, Y')
+                            ->color('gray'),
+                        
+                        TextEntry::make('email_verified_at')
+                            ->translateLabel()
+                            ->label('Email Verification')
+                            ->icon('heroicon-o-check-circle')
+                            ->dateTime('F j, Y g:i A')
+                            ->color('success')
+                            ->placeholder(__('Not Verified'))
+                            ->badge()
+                    ])
+                    ->columns(2),
+
+                Section::make(__('Account Statistics'))
+                    ->translateLabel()
+                    ->icon('heroicon-o-chart-bar')
+                    ->collapsible()
+                    ->collapsed()
+                    ->schema([
+                        \Filament\Infolists\Components\Grid::make(3)
+                            ->schema([
+                                TextEntry::make('updated_at')
+                                    ->translateLabel()
+                                    ->label('Last Updated')
+                                    ->icon('heroicon-o-clock')
+                                    ->since()
+                                    ->color('gray'),
+                                
+                                TextEntry::make('id')
+                                    ->translateLabel()
+                                    ->label('User ID')
+                                    ->icon('heroicon-o-hashtag')
+                                    ->color('gray'),
+                                
+                                TextEntry::make('roles')
+                                    ->translateLabel()
+                                    ->label('Total Roles')
+                                    ->icon('heroicon-o-users')
+                                    ->formatStateUsing(fn ($record) => $record->roles->count())
+                                    ->color('primary'),
+                            ]),
+                    ]),
+            ]);
     }
 
 
